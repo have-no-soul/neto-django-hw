1e08e3ae346e317b21a4d076640c4d7747e290a8
Prokhorov Viktor <vagabund.lpv@gmail.com>
2022-06-02 20:09:11 +0300

    DjangoORM Homework 1

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -74,6 +74,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
+        'USER': 'django_orm_1',
+        'PASSWORD': 'django_orm_1',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Moscow'
 
