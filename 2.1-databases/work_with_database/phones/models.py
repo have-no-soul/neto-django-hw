e12d8bd114496b0a099e3f83c7758f5daeca2c4b
Prokhorov Viktor <vagabund.lpv@gmail.com>
2022-06-02 18:29:08 +0300

    DjangoORM Homework-1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField()
+    release_date = models.TextField()
+    lte_exists = models.TextField()
+    slug = models.TextField()
