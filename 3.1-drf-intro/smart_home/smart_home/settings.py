9b9ab0b4ecfca81aedfe73ed8430d1fb10184aff
Prokhorov Viktor <vagabund.lpv@gmail.com>
2022-06-03 16:16:58 +0300

    update setting.py

--- 3.1-drf-intro/smart_home/smart_home/settings.py
+++ 3.1-drf-intro/smart_home/smart_home/settings.py
@@ -81,8 +81,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_smart_home',
-        'USER': 'postgres',
-        'PASSWORD': 'rfhfvsckjbdfyf',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
