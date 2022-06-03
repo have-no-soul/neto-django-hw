8e08e9c9ee045942dbee0d133b6823fb90829d0d
Prokhorov Viktor <vagabund.lpv@gmail.com>
2022-06-03 20:18:47 +0300

    DjangoPyTest Homework

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -81,6 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_django_testing',
+        'USER': 'postgres',
+        'PASSWORD': 'rfhfvsckjbdfyf',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
