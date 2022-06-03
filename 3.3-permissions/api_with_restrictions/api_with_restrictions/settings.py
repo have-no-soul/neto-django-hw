1c79a82cdff7ea470932ce2f55492e2058ba5532
Prokhorov Viktor <vagabund.lpv@gmail.com>
2022-06-03 19:27:32 +0300

    DjangoPermissions Homework

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -94,8 +94,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_classified_ads',
-        'USER': 'postgres',
-        'PASSWORD': 'rfhfvsckjbdfyf',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
